89319aa73f2fe997590bfe0dc60e8e3bdcc0474a
unknown <drbbeers@gmail.com>
2023-10-08 14:40:30 -0400

    updated to compliant example of R04

--- src/main/java/R04_STR03_J.java
+++ src/main/java/R04_STR03_J.java
@@ -4,12 +4,12 @@ public class R04_STR03_J {
 
     public static void main(String []args){
 
-        //noncompliant solution, attemps to convert bigint to string
+        // compliant solution, uses valid character data to create the string
         BigInteger x = new BigInteger("530500452766");
-        byte[] byteArray = x.toByteArray();
-        String s = new String(byteArray);
-        byteArray = s.getBytes();
-        x = new BigInteger(byteArray);
+        String s = x.toString();  // Valid character data
+        byte[] byteArray = s.getBytes();
+        String ns = new String(byteArray);
+        x = new BigInteger(ns);
     }
 
 }
