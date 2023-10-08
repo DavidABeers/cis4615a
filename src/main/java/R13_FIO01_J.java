265bedf1c044b598eb5e038c078d4f58c24b104d
unknown <drbbeers@gmail.com>
2023-10-08 15:11:04 -0400

    updated to compliant example for R13_FIO01

--- src/main/java/R13_FIO01_J.java
+++ src/main/java/R13_FIO01_J.java
@@ -1,9 +1,33 @@
-import java.io.FileWriter;
-import java.io.Writer;
+import java.io.File;
+import java.nio.channels.SeekableByteChannel;
+import java.nio.file.Files;
+import java.nio.file.OpenOption;
+import java.nio.file.StandardOpenOption;
+import java.nio.file.attribute.FileAttribute;
+import java.nio.file.attribute.PosixFilePermission;
+import java.nio.file.attribute.PosixFilePermissions;
+import java.util.HashSet;
+import java.util.Set;
 
-// noncompliant, access permissions are implementation-defined
+// compliant, uses java.nio classes to manage file permissions
 public class R13_FIO01_J {
     public static void main(String []args){
-        Writer out = new FileWriter("file");
+        WPath file = new File("file").toPath();
+
+// Throw exception rather than overwrite existing file
+        Set<OpenOption> options = new HashSet<OpenOption>();
+        options.add(StandardOpenOption.CREATE_NEW);
+        options.add(StandardOpenOption.APPEND);
+
+// File permissions should be such that only user may read/write file
+        Set<PosixFilePermission> perms =
+                PosixFilePermissions.fromString("rw-------");
+        FileAttribute<Set<PosixFilePermission>> attr =
+                PosixFilePermissions.asFileAttribute(perms);
+
+        try (SeekableByteChannel sbc =
+                     Files.newByteChannel(file, options, attr)) {
+            // Write data
+        };
     }
 }
