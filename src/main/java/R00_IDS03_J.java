0cbf96b56aee979008e61116c9f310fff525fdc8
unknown <drbbeers@gmail.com>
2023-10-08 14:26:59 -0400

    removed noncompliant code from R00

--- src/main/java/R00_IDS03_J.java
+++ src/main/java/R00_IDS03_J.java
@@ -2,17 +2,6 @@ import java.util.logging.Logger;
 
 public class R00_IDS03_J {
 
-    // takes unauthorized user input without data sanitization
-    public void noncomplaintExample(String username, boolean loginSuccessful){
-
-        if(loginSuccessful) {
-            logger.severe("User login succeeded for: " + username);
-        }
-        else {
-            logger.severe("User login failed for: " + username);
-        }
-    }
-
     // sanitizes the user input to prevent injection attacks
     public String sanitizeUser(String username) {
         return Pattern.matches("[A-Za-z0-9_]+", username))
