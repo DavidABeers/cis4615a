028464f00c2cb0591e2708739f58d284a36d3d65
unknown <drbbeers@gmail.com>
2023-10-08 14:17:40 -0400

    added compliant solution too R00

--- src/main/java/R00_IDS03_J.java
+++ src/main/java/R00_IDS03_J.java
@@ -13,4 +13,17 @@ public class R00_IDS03_J {
         }
     }
 
+    // sanitizes the user input to prevent injection attacks
+    public String sanitizeUser(String username) {
+        return Pattern.matches("[A-Za-z0-9_]+", username))
+      ? username : "unauthorized user";
+    }
+    public void compliantExample(String username, boolean loginSuccessful){
+        if (loginSuccessful) {
+            logger.severe("User login succeeded for: " + sanitizeUser(username));
+        } else {
+            logger.severe("User login failed for: " + sanitizeUser(username));
+        }
+    }
+
 }
