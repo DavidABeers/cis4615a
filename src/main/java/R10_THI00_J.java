79be304536fd898d20ae4ae74838970bdb2a55df
unknown <drbbeers@gmail.com>
2023-10-08 15:01:25 -0400

    added noncompliant example for R10_THI00

---
+++ src/main/java/R10_THI00_J.java
@@ -0,0 +1,12 @@
+// Noncompliant, explicitly invokes Thread.run()
+
+public final class Foo implements Runnable {
+    @Override public void run() {
+        // ...
+    }
+
+    public static void main(String[] args) {
+        Foo foo = new Foo();
+        new Thread(foo).run();
+    }
+}
